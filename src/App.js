0ca8e3fe1df81743c556a1e753f9e70aebdb7713
August R <aggesoft@gmail.com>
2021-09-07 18:02:55 +0200

    hotfix ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Editor from './Components/Editor';
 
 function App() {
