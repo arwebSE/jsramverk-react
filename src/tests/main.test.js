8d85b931002010eb68f57f357601844fa2512c46
August R <aggesoft@gmail.com>
2022-01-27 13:07:09 +0100

    revert upgrade

---
+++ src/tests/main.test.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Login from '../routes/Login.jsx';
+
+test('renders learn react link', () => {
+    render(<Login />);
+    const linkElement = screen.getByText(/Login To Your Account/i);
+    expect(linkElement).toBeInTheDocument();
+});
