ae2997e0ee9651f4e4ec85125805bfd01ce01647
August R <aggesoft@gmail.com>
2022-01-27 13:10:35 +0100

    test fix

--- src/test/main.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from '../routes/Login.jsx';
-
-test('renders learn react link', () => {
-    render(<Login />);
-    const linkElement = screen.getByText(/Login To Your Account/i);
-    expect(linkElement).toBeInTheDocument();
-});
